d6772cd91da64dd1cc320f5cefec91089f2848c3
mohitpansari7 <mohitpansari7@gmail.com>
2024-08-17 12:14:48 +0530

    getData code skeleton

---
+++ get_data.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+
+import certifi
+
+import pandas as pd
+import numpy as np
+
+import pymongo
+
+from Network_Security.exception.exception import NetworkSecurityException
+from Network_Security.logger.logger import logging
+
+class NetworkDataExtraction():
+    def __init__():
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def csv_to_json_converter(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    def push_data_to_mongo(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    if __name__ = "__main__":
+        pass
