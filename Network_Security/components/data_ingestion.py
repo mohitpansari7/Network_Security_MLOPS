6b7e200aed40da9082e1a10eab8643c1a2777a9a
mohitpansari7 <mohitpansari7@gmail.com>
2024-08-18 11:05:45 +0530

    data ingestion structure

---
+++ Network_Security/components/data_ingestion.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+
+import pymongo
+from typing import List
+
+from Network_Security.exception.exception import NetworkSecurityException
+from Network_Security.logger.logger import logging
+
+from Network_Security.entity.artifact_entity import DataIngestionArtifact
+
+
+
+from dotenv import load_dotenv
+load_dotenv()
+MONGO_DB_URL = os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
+
+class DataIngestion:
+    def __init__(self) -> None:
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    def export_collection_as_dataframe(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    def export_data_to_feature_store(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def split_data_as_train_test(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def initiate_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
